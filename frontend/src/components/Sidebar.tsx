bfe4e1d3568b0c51c4d147c8ae24f38fe138d155
Nefta Hernandez <neftaligow01@gmail.com>
2024-06-12 10:52:20 -0600

    feat: Add ThemeProvider and MaxWidthWrapper components
    Añado la sidebar y solo haría faltar hacer la función de colapsar

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Apple, MenuIcon, Percent, User } from "lucide-react";
+import { ModeToggle } from "./ModeToggle";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="flex flex-col h-[98vh] w-52 p-5 m-2 rounded-lg border shadow-2xl justify-between">
+      <div>
+        <div className="mb-12">
+          <Link href="/" className="font-bold text-xl flex gap-2 items-center hover:cursor-pointer">
+            <Apple className="w-6 h-6" /> FastFood
+          </Link>
+        </div>
+        <nav className="flex flex-col gap-y-4">
+          <Link
+            href="/menu"
+            className="text-lg flex gap-2 items-center hover:cursor-pointer"
+          >
+            <MenuIcon className="w-5 h-5" /> Menú
+          </Link>
+          <Link
+            href="/promociones"
+            className="text-lg flex gap-2 items-center hover:cursor-pointer"
+          >
+            <Percent className="w-5 h-5" /> Promociones
+          </Link>
+        </nav>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <ModeToggle />
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex items-center border w-full py-2 pl-2 gap-2 rounded-md justify-center">
+            <User className="w-5 h-5" />
+            User
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Mi cuenta</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Perfil</DropdownMenuItem>
+            <DropdownMenuItem>Notificaciones</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
