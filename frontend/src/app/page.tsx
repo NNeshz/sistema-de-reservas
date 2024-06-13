e4d488a877ec80a5f0e4ed1c415a259bcb795c27
Nefta Hernandez <neftaligow01@gmail.com>
2024-06-13 16:11:18 -0600

    feat: Add lazy loading for images and update landing page with BentoGrid component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,16 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { BentoGrid, BentoCard } from "@/components/magicui/BentoGrid";
+import { landingPageData } from "@/json/fakeData";
 
 export default function Home() {
   return (
     <>
-      <MaxWidthWrapper>
-        Hola
+      <MaxWidthWrapper className="my-2">
+        <BentoGrid>
+          {landingPageData.map((item) => (
+            <BentoCard {...item} key={item.name} />
+          ))}
+        </BentoGrid>
       </MaxWidthWrapper>
     </>
   );
---
+++ frontend/src/json/fakeData.ts
@@ -0,0 +1,65 @@
+import { Instagram, BookAIcon, BookOpenIcon, PercentIcon, Users2Icon, Clock } from "lucide-react";
+
+export const landingPageData = [
+    {
+      name: "Redes Sociales",
+      className: "col-span-3 lg:col-span-1",
+      background:
+        "https://images.unsplash.com/photo-1589270018935-ae7e307e79b6?q=80&w=1688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      Icon: Instagram,
+      description: "No te pierdas ninguna de nuestras actualizaciones.",
+      href: "/",
+      cta: "Conoce más",
+    },
+    {
+      name: "Blog",
+      className: "col-span-3 lg:col-span-2",
+      Icon: BookAIcon,
+      background:
+        "https://images.unsplash.com/photo-1545239351-ef35f43d514b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description: "Descubre los mejores consejos para tu salud y bienestar.",
+      href: "/",
+      cta: "Leer más",
+    },
+    {
+      name: "Menu",
+      description:
+        "Conoce nuestra carta y descubre los platillos que tenemos para ti.",
+      href: "/",
+      cta: "Ver menú",
+      Icon: BookOpenIcon,
+      className: "col-span-3 lg:col-span-2",
+      background:
+        "https://images.unsplash.com/photo-1568031813264-d394c5d474b9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWVudXxlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+      name: "Promociones",
+      description: "Descubre nuestras promociones y descuentos.",
+      href: "/",
+      cta: "Ver promociones",
+      Icon: PercentIcon,
+      className: "col-span-3 lg:col-span-1",
+      background:
+        "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb2R8ZW58MHx8MHx8fDA%3D",
+    },
+    {
+      name: "Nosotros",
+      description: "Conoce más sobre nosotros y nuestro equipo.",
+      href: "/",
+      cta: "Conoce más",
+      Icon: Users2Icon,
+      className: "col-span-3 lg:col-span-1",
+      background:
+        "https://images.unsplash.com/photo-1543269865-cbf427effbad?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Reservaciones",
+      description: "Reserva tu mesa y disfruta de una experiencia única.",
+      href: "/",
+      cta: "Reservar",
+      Icon: Clock,
+      className: "col-span-3 lg:col-span-2",
+      background:
+        "https://images.unsplash.com/photo-1634234498573-29224acf2907?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVzZXJ2YXRpb258ZW58MHx8MHx8fDA%3D",
+    },
+  ];
