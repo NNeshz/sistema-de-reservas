8272e1f85b19d9d9cf23f807792a716d06036a50
ExalGitHub <ExequielAlmonacid2020@gmail.com>
2024-06-16 03:08:36 -0300

    modified:   backend/authorization/urls.py 	modified:   backend/authorization/views.py 	modified:   backend/backend/settings.py 	modified:   backend/db.sqlite3 	new file:   backend/restaurant/menu_views.py 	new file:   backend/restaurant/table_views.py 	modified:   backend/restaurant/urls.py 	modified:   backend/restaurant/views.py

--- backend/authorization/urls.py
+++ backend/authorization/urls.py
@@ -1,15 +1,12 @@
-# from django.contrib import admin
-from django.urls import path, re_path
-
+from django.urls import re_path
 from .views import login, logout, profile, register, delete_user, update_user, staff
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    re_path('login'   , login),             #Post
-    re_path('logout'  , logout),            #Post
-    re_path('register', register),          #Post
-    re_path('profile' , profile),           #Get
-    re_path('delete_user' , delete_user),   #Delete
-    re_path('update_user' , update_user),   #Put
-    re_path('staff'   , staff),             #Post
+    re_path('user/login/'   , login),             #Post
+    re_path('user/logout/'  , logout),            #Post
+    re_path('user/create/'  , register),          #Post
+    re_path('user/profile/' , profile),           #Get
+    re_path('user/delete/'  , delete_user),       #Delete
+    re_path('user/update/'  , update_user),       #Put
+    re_path('staff-only/'   , staff),             #Post
 ]
---
+++ backend/restaurant/table_views.py
@@ -0,0 +1,20 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import viewsets
+from .permissions import IsStaffOrReadOnly
+from .serializers import TableSerializer
+from .models import Table
+
+
+class TableViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsStaffOrReadOnly]
+    
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
+    
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
