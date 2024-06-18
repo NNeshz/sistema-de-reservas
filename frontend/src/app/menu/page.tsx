31a8e61b765ffcbadaa49e2d2d67940681382683
Nefta Hernandez <neftaligow01@gmail.com>
2024-06-18 09:11:58 -0600

    feat: Add CategoryProductPage component for displaying category-specific products

--- frontend/src/app/menu/page.tsx
+++ frontend/src/app/menu/page.tsx
@@ -1,9 +1,55 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { categoriesData } from "@/json/fakeData";
+import Image from "next/image";
+import Link from "next/link";
+
 const MenuPage = () => {
-    return (
-        <div>
-            <h1>Menu</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <MaxWidthWrapper className="px-4 mb-14">
+        <section className="mt-4 md:my-10">
+          <p className="text-xl font-semibold md:text-2xl">¡Disfruta!</p>
+          <h2 className="text-3xl font-bold md:text-6xl">
+            Conoce nuestro menú de{" "}
+            <p className="text-yellow-400">alimentos y bebidas.</p>
+          </h2>
+        </section>
+        <section>
+          {Object.entries(categoriesData).map(([category, items]) => (
+            <div key={category} className="mt-4">
+              <h3 className="text-xl font-semibold rounded-xl my-2 md:text-4xl md:mt-12">
+                {category}
+              </h3>
+              <ul className="grid grid-cols-2 gap-8 md:grid-cols-3">
+                {items.map((item) => (
+                  <li key={item.name} className="rounded-md">
+                    <Link
+                      href={item.href}
+                    >
+                      <AspectRatio ratio={1 / 1}>
+                        <Image
+                          src={item.image}
+                          alt="Dish 1"
+                          fill
+                          objectFit="cover"
+                          quality={100}
+                          className="rounded-lg"
+                        />
+                      </AspectRatio>
+                    </Link>
+                    <h4 className="text-center mt-2 font-semibold md:text-2xl ">
+                      {item.name}
+                    </h4>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </section>
+      </MaxWidthWrapper>
+    </div>
+  );
+};
 
 export default MenuPage;
--- frontend/src/json/fakeData.ts
+++ frontend/src/json/fakeData.ts
@@ -1,89 +1,174 @@
-import { Instagram, BookAIcon, BookOpenIcon, PercentIcon, Users2Icon, Clock } from "lucide-react";
-
-export const landingPageData = [
-    {
-      name: "Redes Sociales",
-      className: "col-span-3 lg:col-span-1",
-      background:
-        "https://images.unsplash.com/photo-1589270018935-ae7e307e79b6?q=80&w=1688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      Icon: Instagram,
-      description: "No te pierdas ninguna de nuestras actualizaciones.",
-      href: "/",
-      cta: "Conoce más",
-    },
-    {
-      name: "Blog",
-      className: "col-span-3 lg:col-span-2",
-      Icon: BookAIcon,
-      background:
-        "https://images.unsplash.com/photo-1545239351-ef35f43d514b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "Descubre los mejores consejos para tu salud y bienestar.",
-      href: "/",
-      cta: "Leer más",
-    },
-    {
-      name: "Menu",
-      description:
-        "Conoce nuestra carta y descubre los platillos que tenemos para ti.",
-      href: "/",
-      cta: "Ver menú",
-      Icon: BookOpenIcon,
-      className: "col-span-3 lg:col-span-2",
-      background:
-        "https://images.unsplash.com/photo-1568031813264-d394c5d474b9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWVudXxlbnwwfHwwfHx8MA%3D%3D",
-    },
-    {
-      name: "Promociones",
-      description: "Descubre nuestras promociones y descuentos.",
-      href: "/",
-      cta: "Ver promociones",
-      Icon: PercentIcon,
-      className: "col-span-3 lg:col-span-1",
-      background:
-        "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb2R8ZW58MHx8MHx8fDA%3D",
-    },
-    {
-      name: "Nosotros",
-      description: "Conoce más sobre nosotros y nuestro equipo.",
-      href: "/",
-      cta: "Conoce más",
-      Icon: Users2Icon,
-      className: "col-span-3 lg:col-span-1",
-      background:
-        "https://images.unsplash.com/photo-1543269865-cbf427effbad?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      name: "Reservaciones",
-      description: "Reserva tu mesa y disfruta de una experiencia única.",
-      href: "/",
-      cta: "Reservar",
-      Icon: Clock,
-      className: "col-span-3 lg:col-span-2",
-      background:
-        "https://images.unsplash.com/photo-1634234498573-29224acf2907?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVzZXJ2YXRpb258ZW58MHx8MHx8fDA%3D",
-    },
-  ];
-
 export const topDishesData = [
   {
     name: "Camarones en Habanero",
     description: "Camarones al ajillo con salsa de habanero.",
-    imgSource: "https://images.unsplash.com/photo-1625943553852-781c6dd46faa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGxhdGlsbG8lMjBkZSUyMGNhbWFyb25lc3xlbnwwfHwwfHx8MA%3D%3D",
+    imgSource:
+      "https://images.unsplash.com/photo-1625943553852-781c6dd46faa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGxhdGlsbG8lMjBkZSUyMGNhbWFyb25lc3xlbnwwfHwwfHx8MA%3D%3D",
     price: 200,
     stars: 4.5,
   },
   {
     name: "Carne Asada",
     description: "Carne asada con guacamole y tortillas de maíz.",
-    imgSource: "https://images.unsplash.com/photo-1625937329935-287441889bce?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imgSource:
+      "https://images.unsplash.com/photo-1625937329935-287441889bce?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     price: 150,
     stars: 4.5,
   },
   {
     name: "Spaghetti Carbonara",
     description: "Spaghetti con salsa carbonara y queso parmesano.",
-    imgSource: "https://images.unsplash.com/photo-1685156329688-ffe9c1a99a06?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imgSource:
+      "https://images.unsplash.com/photo-1685156329688-ffe9c1a99a06?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     price: 100,
-    stars: 4.7
-  }
-]
+    stars: 4.7,
+  },
+];
+
+export const categoriesData = {
+  Bebidas: [
+    {
+      name: "Bebidas calientes",
+      image:
+        "https://images.unsplash.com/photo-1518057111178-44a106bad636?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      href: "/menu/bebidas-calientes",
+    },
+    {
+      name: "Bebidas frías",
+      image:
+        "https://images.unsplash.com/photo-1518057111178-44a106bad636?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      href: "/menu/bebidas-frias",
+    },
+    {
+      name: "Base de Té",
+      image:
+        "https://images.unsplash.com/photo-1518057111178-44a106bad636?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      href: "/menu/base-de-te",
+    },
+  ],
+  Alimentos: [
+    {
+      name: "Desayunos",
+      image:
+        "https://images.unsplash.com/photo-1525351484163-7529414344d8?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      href: "/menu/desayunos",
+    },
+    {
+      name: "Comidas",
+      image:
+        "https://images.unsplash.com/photo-1525351484163-7529414344d8?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      href: "/menu/comidas",
+    },
+    {
+      name: "Cenas",
+      image:
+        "https://images.unsplash.com/photo-1525351484163-7529414344d8?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      href: "/menu/cenas",
+    },
+    {
+      name: "Postres",
+      image:
+        "https://images.unsplash.com/photo-1525351484163-7529414344d8?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      href: "/menu/postres",
+    },
+    {
+      name: "Snacks",
+      image:
+        "https://images.unsplash.com/photo-1525351484163-7529414344d8?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      href: "/menu/snacks",
+    },
+  ],
+};
+
+export const allDishesData = [
+  {
+    name: "Esspresso",
+    description: "Café cargado con un intenso sabor y aroma.",
+    category: "Bebidas calientes",
+    price: 30,
+    stars: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Esspresso",
+    description: "Café cargado con un intenso sabor y aroma.",
+    category: "Bebidas calientes",
+    price: 30,
+    stars: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Esspresso",
+    description: "Café cargado con un intenso sabor y aroma.",
+    category: "Bebidas calientes",
+    price: 30,
+    stars: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Esspresso",
+    description: "Café cargado con un intenso sabor y aroma.",
+    category: "Bebidas calientes",
+    price: 30,
+    stars: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Esspresso",
+    description: "Café cargado con un intenso sabor y aroma.",
+    category: "Bebidas calientes",
+    price: 30,
+    stars: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Esspresso",
+    description: "Café cargado con un intenso sabor y aroma.",
+    category: "Bebidas calientes",
+    price: 30,
+    stars: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Esspresso",
+    description: "Café cargado con un intenso sabor y aroma.",
+    category: "Bebidas calientes",
+    price: 30,
+    stars: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Esspresso",
+    description: "Café cargado con un intenso sabor y aroma.",
+    category: "Bebidas calientes",
+    price: 30,
+    stars: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Esspresso",
+    description: "Café cargado con un intenso sabor y aroma.",
+    category: "Bebidas calientes",
+    price: 30,
+    stars: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Esspresso",
+    description: "Café cargado con un intenso sabor y aroma.",
+    category: "Bebidas calientes",
+    price: 30,
+    stars: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
