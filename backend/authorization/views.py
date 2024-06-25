9ca1825aba2e8634d8be3fca2dc69205a8ae455a
ExalGitHub <ExequielAlmonacid2020@gmail.com>
2024-06-25 08:26:52 -0300

    modified:   backend/authorization/views.py

--- backend/authorization/views.py
+++ backend/authorization/views.py
@@ -6,7 +6,7 @@ from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
-from drf_spectacular.utils import extend_schema, OpenApiResponse
+from drf_spectacular.utils import extend_schema
 
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
